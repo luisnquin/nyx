f8b308769550f04f196311c390da06e85158e5d6
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-02-05 15:29:16 -0500

    feat: added default.nix

---
+++ default.nix
@@ -0,0 +1,31 @@
+{pkgs ? import <nixpkgs> {}}: let
+  owner = "luisnquin"; # Yeah, I need flakes
+in
+  pkgs.stdenv.mkDerivation rec {
+    name = "nyx";
+    src = pkgs.fetchFromGitHub {
+      owner = owner;
+      repo = name;
+      rev = "fd2612b998043dd025f0fbb30da74bb2ef2c605d";
+      sha256 = "1si58criblgv9r65agynglrdxgyszpv0rydmssmj2r9ghy3wvnii";
+    };
+
+    installPhase = ''
+      mkdir -p $out/bin/
+      cp $src/nyx.sh $out/bin/
+      mv $out/bin/nyx.sh $out/bin/nyx
+      chmod +x $out/bin/nyx
+    '';
+
+    propagatedBuildInputs = with pkgs; [
+      alejandra
+      exa
+    ];
+
+    meta = with pkgs.lib; {
+      description = "A CLI tool to manage NixOS computers";
+      homepage = "https://github.com/${owner}/${name}";
+      license = licenses.unlicense;
+      maintainers = with maintainers; [luisnquin];
+    };
+  }
